ef72298473ab8af38412eba4930588a80b02bc20
Mamelq <p.mamelka99@wp.pl>
2023-10-16 12:49:05 +0200

    functions

--- readme.md
+++ readme.md
@@ -8,3 +8,12 @@ redeclaration | + (quarks mode) |    -    | -
 re-assign | + |    +    | - 
 hosting | + | + (TDZ) | + (TDZ)
 scope | function |  block  | block
+
+---
+## Functions
+1. Named Functions
+2. Anonymus Functions
+   - classic
+   - arrow function 
+
+![functions.png](functions.png)
--- src/scripts/variables.js
+++ src/scripts/variables.js
@@ -1,15 +1,14 @@
 // Redeklaracja zmiennych
-// Deklaracja zmiennej to stwoerzenie jej za pomocą np. let x;
+// Deklaracja zmiennej to stworzenie jej za pomocą np. let x;
 
 // var x1 = 42;
 // var x1 = 666;
 //
 // let x2 = 42;
-// let x2 = 666; // nie można re deklarować, błąd
+// let x2 = 666;  // nie można re deklarować, błąd
 //
 // const x3 = 42;
 // const x3 = 666; // nie można re deklarować, błąd
-
 
 // Reinicjalizacja
 // Przypisanie nowej wartości do istniejącej zmiennej np. x = 42;
@@ -21,42 +20,61 @@
 // x2 = 666;
 //
 // const x3 = 42;
-// x3 = 666;
+// x3 = 666; // const nie można reinicjalizować
 
 
 // Hoisting
-// Przenoszenie deklaracji zmiennyych var, deklaracji funkcji nazywanych deklaracji klass
-// na górę aktualnie przetwarzeanego zasięgu. TDZ - temporary dead zone (tymczasowa strefa smierci)
+// Przenoszenie deklaracji zmiennych, deklaracji funkcji nazwanych, deklaracji klas
+// na górę aktualnie przetwarzanego zasięgu. TDZ — temporary dead zone (tymczasowa strefa śmierci)
 
-// Scope - zasięg
-// Widoczność, dostępność identyfikatorów (nazwy zmiennych, funkcji i klass)
-// - global - wszędzie jest coś dostępne
-// - local - dostępne tylko w aktualnym zasięgu
-// - function scope - tworzymy przez ciało fuinkcji
+function doSmthAwesome() {
+    var z1;
+    console.log(z1);
+    z1 = 42;
+}
+
+doSmthAwesome()
+
+// Scope — zasięg
+// Widoczność, dostępność identyfikatorów (nazwy zmiennych, funkcji i klas)
+// - global — wszędzie jest coś dostępne
+// — local — dostępne tylko w aktualnym zasięgu
+// - function scope — tworzony przez ciało funkcji
 // - block scope - tworzony przez block, dowolna para 2 klamerek
 
-// function doSmth (){
-//     var x1 = 42;
-//     let x2 = 42;
-//     const x3 = 42;
-// }
-//
-// // console.log (x1, x2, x3) są niedostępne w aktualnym scope, bo są zadeklarowane wewnątrz funkcji
-//
+function doSmth() {
+    var x1 = 42;
+    let x2 = 42;
+    const x3 = 42;
+}
+
+doSmth();
+// console.log(x1, x2, x3) są niedostępne w aktualnym scope, bo są zadeklarowane wewnątrz funkcji
+
 // if (true) {
 //     var x1 = 42;
 //     let x2 = 42;
 //     const x3 = 42;
 // }
-// console.log (x1);
-// console.log (x1, x2, x3);
+//
+// console.log(x1);
+// console.log(x1, x2, x3);
 
 
 // let x = 42;
+//
 // function doSmthMagic(){
-//     x = 666; //kiedy nie ma deklaracji zmiennej to szuka w zasięgu otaczającym / wyżej
-//     let x = 666; //kiedy jest deklaracjam to jest tworzona nowa zmiennea, a ta wyżej nie jest używana
+//     x = 666; // kiedy nie ma deklaracji zmiennej to szuka w zasięgu otaczającym / wyżej
+//     // let x = 666; // kiedy jest deklaracja, to jest tworzona nowa zmienna, a ta wyżej nie jest używana
 //     console.log(x)
 // }
+//
 // doSmthMagic();
 // console.log(x);
+
+
+// for (let i = 0; i < 10; i++){
+//     setTimeout(function () {
+//         console.log(i);
+//     }, 0)
+// }
